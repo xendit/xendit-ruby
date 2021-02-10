0cbcc09284d75a3bd20b9b599ccd8295c43f3b05
Benjamin Toh <benjamin@xendit.co>
2021-02-11 04:01:39 +0800

    Fixed code style

--- lib/xendit/resources/invoice.rb
+++ lib/xendit/resources/invoice.rb
@@ -12,7 +12,7 @@ module Xendit
       def get(invoice_id)
         # validation
         raise ArgumentError, 'invoice_id is required and should be a string' \
-            if invoice_id.nil? or !invoice_id.is_a?(String)
+            if invoice_id.nil? || !invoice_id.is_a?(String)
 
         resp = Xendit::ApiOperations.get "v2/invoices/#{invoice_id}"
         Xendit::Response.handle_error_response resp
@@ -20,23 +20,23 @@ module Xendit
         JSON.parse resp.body
       end
 
-      # rubocop:disable Metrics/CyclomaticComplexity, Metrics/AbcSize, Metrics/MethodLength, Metrics/PerceivedComplexity
+      # rubocop:disable Metrics/CyclomaticComplexity, Metrics/AbcSize, Metrics/PerceivedComplexity
       def create(invoice_params)
         # validation
         raise ArgumentError, 'invoice body payload is required' \
-            if invoice_params.nil? or invoice_params.empty?
+            if invoice_params.nil? || invoice_params.empty?
         raise ArgumentError, 'payer_email is required and should be a string' \
-            if invoice_params[:payer_email].nil? or
+            if invoice_params[:payer_email].nil? ||
                !invoice_params[:payer_email].is_a?(String)
         raise ArgumentError, 'external_id is required and should be a string' \
-            if invoice_params[:external_id].nil? or
+            if invoice_params[:external_id].nil? ||
                !invoice_params[:external_id].is_a?(String)
         raise ArgumentError, 'description is required and should be a string' \
-            if invoice_params[:description].nil? or
+            if invoice_params[:description].nil? ||
                !invoice_params[:description].is_a?(String)
         raise ArgumentError, 'amount is required and should be an integer or float' \
-            if invoice_params[:amount].nil? or
-               (!invoice_params[:amount].is_a?(Integer) and
+            if invoice_params[:amount].nil? ||
+               (!invoice_params[:amount].is_a?(Integer) &&
                !invoice_params[:amount].is_a?(Float))
 
         resp = Xendit::ApiOperations.post('v2/invoices', **invoice_params)
@@ -44,12 +44,12 @@ module Xendit
 
         JSON.parse resp.body
       end
-      # rubocop:enable Metrics/CyclomaticComplexity, Metrics/AbcSize, Metrics/MethodLength, Metrics/PerceivedComplexity
+      # rubocop:enable Metrics/CyclomaticComplexity, Metrics/AbcSize, Metrics/PerceivedComplexity
 
       def expire(invoice_id)
         # validation
         raise ArgumentError, 'invoice_id is required and should be a string' \
-            if invoice_id.nil? or !invoice_id.is_a?(String)
+            if invoice_id.nil? || !invoice_id.is_a?(String)
 
         resp = Xendit::ApiOperations.post "invoices/#{invoice_id}/expire!"
         Xendit::Response.handle_error_response resp
