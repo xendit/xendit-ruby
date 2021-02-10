d954bc801320eeb8adec264585364de165083cdc
Benjamin Toh <benjamin@xendit.co>
2021-02-11 04:20:05 +0800

    Added rescue JSON::ParserError to handle_error_response

--- lib/xendit/response.rb
+++ lib/xendit/response.rb
@@ -5,18 +5,24 @@ require_relative './errors'
 module Xendit
   class Response
     class << self
+      # rubocop:disable Metrics/AbcSize
       def handle_error_response(resp)
         # validation
         raise ArgumentError, 'response is required' \
             if resp.nil?
         raise ArgumentError, 'response is in invalid format' \
-            if resp.status.nil? && resp.body.nil?
+            if resp.status.nil? || resp.body.nil?
 
         return unless resp.status >= 400
 
-        body = JSON.parse resp.body
-        raise Xendit::APIError.new(body['error_code'], body['message'], resp.status)
+        begin
+          body = JSON.parse resp.body
+          raise Xendit::APIError.new(body['error_code'], body['message'], resp.status)
+        rescue JSON::ParserError
+          raise Xendit::APIError.new('INVALID_RESPONSE_BODY_FORMAT', 'Response body is not in JSON format', 422)
+        end
       end
+      # rubocop:enable Metrics/AbcSize
     end
   end
 end
--- spec/xendit/response_spec.rb
+++ spec/xendit/response_spec.rb
@@ -4,27 +4,6 @@ require 'xendit/response'
 
 describe Xendit::Response do
   describe '.create_connection' do
-    let(:error_response) do
-      OpenStruct.new({
-                       status: 400,
-                       body: '{"error_code":"INVALID_API_KEY","message":"API key is not authorized for this API service"}'
-                     })
-    end
-
-    let(:invalid_response) do
-      OpenStruct.new({
-                       invalid_status: 200,
-                       invalid_body: '{"id":"1273018302173","status":"PENDING"}'
-                     })
-    end
-
-    let(:valid_response) do
-      OpenStruct.new({
-                       status: 200,
-                       body: '{"id":"1273018302173","status":"PENDING"}'
-                     })
-    end
-
     context 'given no response' do
       it 'should raise argument error' do
         expect(-> { described_class.handle_error_response }).
@@ -33,23 +12,58 @@ describe Xendit::Response do
     end
 
     context 'given an invalid response' do
+      let(:invalid_response) do
+        OpenStruct.new({
+                         invalid_status: 200,
+                         invalid_body: '{"id":"1273018302173","status":"PENDING"}'
+                       })
+      end
+
       it 'should raise argument error' do
         expect(-> { described_class.handle_error_response(invalid_response) }).
           to raise_error(ArgumentError)
       end
     end
 
-    context 'given a valid response' do
-      it 'should not raise API error' do
-        expect(-> { described_class.handle_error_response(valid_response) }).
-          not_to raise_error(Xendit::APIError)
+    context 'given an invalid body response' do
+      let(:invalid_body_response) do
+        OpenStruct.new({
+                         status: 400,
+                         body: 'this is not a json'
+                       })
+      end
+
+      it 'should raise API error' do
+        expect(-> { described_class.handle_error_response(invalid_body_response) }).
+          to raise_error(Xendit::APIError)
       end
     end
 
     context 'given an error response' do
+      let(:error_response) do
+        OpenStruct.new({
+                         status: 400,
+                         body: '{"error_code":"INVALID_API_KEY","message":"API key is not authorized for this API service"}'
+                       })
+      end
+
       it 'should raise API error' do
         expect(-> { described_class.handle_error_response(error_response) }).
           to raise_error(Xendit::APIError)
+      end
+    end
+
+    context 'given a valid response' do
+      let(:valid_response) do
+        OpenStruct.new({
+                         status: 200,
+                         body: '{"id":"1273018302173","status":"PENDING"}'
+                       })
+      end
+
+      it 'should not raise API error' do
+        expect(-> { described_class.handle_error_response(valid_response) }).
+          not_to raise_error(Xendit::APIError)
       end
     end
   end
